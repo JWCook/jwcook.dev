70a942a175607635802378ca45ae937886548bc2
Jordan Cook <jordan.cook.git@proton.me>
2024-07-11 16:27:16 -0500

    Fix paths in dither script, expand palette

--- scripts/dither.sh
+++ scripts/dither.sh
@@ -2,9 +2,9 @@
 # Default settings for dithering
 # Uses the didder CLI tool: https://github.com/makew0rld/didder
 set -e
-
-OUT_FILE="assets/images/${1%.*}.png"
-PALETTE='#1d2021 #3c3836 #665c54
+FILENAME=$(basename "$1")
+OUT_FILE="assets/images/${FILENAME%.*}.png"
+PALETTE='#1d2021 #504945 #3c3836 #665c54
  #928374 #f9f5d7 #fbf1c7 #f2e5bc #d5c4a1 #a89984
  #fb4934 #b8bb26 #fabd2f #83a598 #d3869b #8ec07c #fe8019 #cc241d
  #98971a #d79921 #458588 #b16286 #689d6a #d65d0e #9d0006 #79740e
